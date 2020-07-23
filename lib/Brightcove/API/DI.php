9836e12fda6aab5b3b8fc4f0f9b7cbaae453d771
Sergiu <sergiu.savva@gmail.com>
2020-07-23 23:44:27 +0300

    Added ingest_jobs endpoints.

--- lib/Brightcove/API/DI.php
+++ lib/Brightcove/API/DI.php
@@ -24,4 +24,18 @@ class DI extends API {
   public function createIngest($video_id, IngestRequest $request) {
     return $this->diRequest('POST', "/videos/{$video_id}/ingest-requests", IngestResponse::class, FALSE, $request);
   }
+
+  /**
+   * @return array
+   */
+  public function getJobs($video_id) {
+    return $this->diRequest('GET', "/videos/{$video_id}/ingest_jobs", null);
+  }
+
+  /**
+   * @return array
+   */
+  public function getJob($video_id, $job_id) {
+    return $this->diRequest('GET', "/videos/{$video_id}/ingest_jobs/{$job_id}", null);
+  }
 }
